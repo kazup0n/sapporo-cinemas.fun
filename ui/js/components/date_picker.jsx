52364f71b913c6a7172733785220b3c5fd7a2bc9
Kazuhiro Sasaki <dusty.trombone@gmail.com>
2018-02-24 23:57:44 +0900

    GAタグとfaviconを追加

--- ui/js/components/date_picker.jsx
+++ ui/js/components/date_picker.jsx
@@ -9,8 +9,8 @@ export default (props) =>{
   }
   return (<form>
     <div className="form-group">
-      <label>Pick a date: </label>
-      <input type="date" className="form-control" onChange={onDateChange} />
+      <label>Pick a date (e.g. 2017-02-24): </label>
+      <input type="date" className="form-control" placeholder="2017-02-24" onChange={onDateChange} />
     </div>
     </form>)
 }
