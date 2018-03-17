d3fc6a8b61c6b6c9d757483c6a23b6c16d10cc4b
kazup0n <dusty.trombone@gmail.com>
2018-03-17 10:54:34 +0900

    extend timeout for runnging on ci

--- functions/test/crawlers/cinema_frontier.js
+++ functions/test/crawlers/cinema_frontier.js
@@ -4,14 +4,14 @@ const cf = require('../../js/crawlers/cinema_frontier')
 
 describe('cinema_frontier', ()=>{
   describe('fetchAll', ()=>{
-    it('returns promised schedules', (done)=>{
-      cf().then((result)=>{
+    it('returns promised schedules', function() {
+      this.timeout(5000)
+      return cf().then((result)=>{
         assert(result.length > 0)
         assert.deepEqual(
           _.sortBy(Object.keys(result[0])),
           _.sortBy(['theater','title','date','schedules'])
         )
-        done()
       })
     })
   })
--- functions/test/crawlers/kino.js
+++ functions/test/crawlers/kino.js
@@ -4,14 +4,14 @@ const kino = require('../../js/crawlers/kino')
 
 describe('kino', ()=>{
   describe('fetchAll', ()=>{
-    it('returns promised schedules', (done)=>{
-      kino().then((result)=>{
+    it('returns promised schedules', function(){
+      this.timeout(5000)
+      return kino().then((result)=>{
         assert(result.length > 0)
         assert.deepEqual(
           _.sortBy(Object.keys(result[0])),
           _.sortBy(['theater','title','date','schedules'])
         )
-        done()
       })
     })
   })
